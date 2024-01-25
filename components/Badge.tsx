08fb759b8bb216bb721a2e8ef9015dfef485c48e
Frank Pizzella <fpizz@Franks-MacBook-Pro.local>
2024-01-24 22:09:23 -0500

    1.0.2: Badge Notifications now bring you to badge page

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -21,16 +21,16 @@ const Badge: React.FC<BadgeProps> = ({ badge, handleBadgePress }) => {
       style={[styles.badge, badge.earned ? styles.goldenBadge : null]}
       onPress={() => handlePress()}
     >
-      <Text style={styles.badgeTitle}>
+      <Text style={styles.badgeTitle} maxFontSizeMultiplier={1.2}>
         {badge.collections.name.toUpperCase()}
       </Text>
-      <Text style={standardStyles.basicCardText}>
+      <Text style={[standardStyles.basicCardText, styles.badgeDescription]} maxFontSizeMultiplier={1.2}>
         {badge.collections.description}
       </Text>
-      <Text style={standardStyles.basicCardText}>
+      <Text style={[standardStyles.basicCardText, styles.badgeText]} maxFontSizeMultiplier={1.2}>
         Difficulty: {badge.collections.difficulty}
       </Text>
-      <Text style={standardStyles.basicCardText}>
+      <Text style={[standardStyles.basicCardText, styles.badgeText]} maxFontSizeMultiplier={1.2}>
         Progress: {decimalToPercent(badge.progress)}
       </Text>
     </TouchableOpacity>
@@ -44,7 +44,8 @@ const styles = StyleSheet.create({
     fontSize: Dimensions.get("window").width / 15,
     fontWeight: "bold",
     justifyContent: "center",
-    marginBottom: 10,
+    textAlign: "center",
+    paddingTop: 5,
   },
   badge: {
     alignItems: "center",
@@ -53,7 +54,14 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     margin: 5,
     width: Dimensions.get("window").width - 50,
-    height: Dimensions.get("window").height / 4,
+    minHeight: Dimensions.get("window").height / 4,
+    padding: 10,
+  },
+  badgeDescription: {
+    padding: 10,
+  },
+  badgeText: {
+    padding: 5,
   },
   goldenBadge: {
     backgroundColor: "gold",
--- screens/NotificationsScreen.tsx
+++ screens/NotificationsScreen.tsx
@@ -8,7 +8,7 @@ import {
   Dimensions,
 } from "react-native";
 import { fetchNotifications, fetchUserByUserName } from "../Models/Requests";
-import { Notification } from "../Models/SQLData";
+import { Notification, NotificationType } from "../Models/SQLData";
 import { NotificationsProps } from "../props";
 import { useNavigation } from "@react-navigation/core";
 import { BackgroundColor } from "../Styles/colors";
@@ -29,16 +29,20 @@ const NotificationsScreen = (props: NotificationsProps) => {
     }, []),
   );
 
-  const handleFriendPress = async (notificationId: number) => {
+  const handleNotificationPress = async (notificationId: number) => {
     const notification = notifications.find(
       (notification) => notification.id === notificationId,
     );
-    const user = await fetchUserByUserName(
-      notification?.message.split(" ")[0]!,
-    );
-    navigation.navigate("FriendProfile", {
-      friend_id: user.id,
-    });
+    if (notification?.type === NotificationType.NEW_FRIEND) {
+      const user = await fetchUserByUserName(
+        notification?.message.split(" ")[0]!,
+      );
+      navigation.navigate("FriendProfile", {
+        friend_id: user.id,
+      });
+    } else if (notification?.type === NotificationType.BADGE_EARNED) {
+      navigation.navigate("YourBadges");
+    }
   };
 
   return (
@@ -51,7 +55,7 @@ const NotificationsScreen = (props: NotificationsProps) => {
           return (
             <TouchableOpacity
               key={notification.id}
-              onPress={() => handleFriendPress(notification.id)}
+              onPress={() => handleNotificationPress(notification.id)}
               style={styles.notification}
             >
               <Text style={styles.message} maxFontSizeMultiplier={1.2}>{notification.message}</Text>
