0b9c0bc433f722613e752db0fa3a1719a9a22209
Frank Pizzella <fpizz210@gmail.com>
2024-03-18 23:33:04 -0400

    Fixed Project ID env varibale name

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -11,7 +11,7 @@ import { auth } from "../Models/firebase";
 import { useNavigation } from "@react-navigation/core";
 import { HomeProps } from "../props";
 import { User } from "../Models/SQLData";
-import { EXPO_PUBLIC_API_URL, EXPO_PROJECT_ID } from "@env";
+import { EXPO_PUBLIC_API_URL, EXPO_PUBLIC_EXPO_PROJECT_ID } from "@env";
 import { BackgroundColor, MainButtonColor, TitleColor } from "../Styles/colors";
 import { Ionicons } from "@expo/vector-icons";
 import { useFocusEffect } from "@react-navigation/native";
@@ -43,7 +43,7 @@ async function registerForPushNotificationsAsync() {
   }
   const token = (
     await Notifications.getExpoPushTokenAsync({
-      projectId: EXPO_PROJECT_ID,
+      projectId: EXPO_PUBLIC_EXPO_PROJECT_ID,
     })
   ).data;
   return token;
