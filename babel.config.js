ab6cd1c8d58c575f0cb736a359aac87fc896182b
Frank Pizzella <fpizz210@gmail.com>
2024-12-09 13:55:08 -0500

    Not working

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,27 @@
 module.exports = function (api) {
   api.cache(true);
+  api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: true,
-        },
-      ],
-    ],
+    presets: [["babel-preset-expo", {
+      jsxImportSource: "nativewind"
+    }], "nativewind/babel"],
+    plugins: [[
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        path: ".env",
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ], ["module-resolver", {
+      root: ["./"],
+
+      alias: {
+        "@": "./",
+        "tailwind.config": "./tailwind.config.js"
+      }
+    }]],
   };
 };
